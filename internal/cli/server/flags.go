0ae5f7f0f019dc8fe3cef24c8d454ddfb23681b7
nebojsa.urosevic <nebojsa94urosevic@gmail.com>
2025-08-12 09:49:08 +0200

    Fix LT flag propagation

--- internal/cli/server/flags.go
+++ internal/cli/server/flags.go
@@ -131,11 +131,11 @@ func (c *Command) Flags(config *Config) *flagset.Flagset {
 		Default: c.cliConfig.Logging.Vmodule,
 		Group:   "Logging",
 	})
-	f.BoolFlag(&flagset.BoolFlag{
+	f.StringFlag(&flagset.StringFlag{
 		Name:    "vmtrace",
 		Usage:   "Name of tracer which should record internal VM operations (costly)",
 		Value:   &c.cliConfig.VMTrace,
-		Default: c.cliConfig.VMTrace,
+		Default: "",
 		Group:   "Logging",
 	})
 	f.StringFlag(&flagset.StringFlag{
